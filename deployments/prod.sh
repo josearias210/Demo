14db624dd52a6dff99ee3efad42574a1f278a426
Your Name <josearias210@gmail.com>
2017-10-24 15:52:55 -0400

    Config project

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,1 +1,4 @@
+wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.1.1/packer_1.1.1_linux_amd64.zip?_ga=2.50621353.1804937557.1508818859-81110963.1508818859
+unizp /tmp/packer.zip -d ~/bin
+packer validate deployments/template_do_prod.json &&
 packer build deployments/template_do_prod.json
